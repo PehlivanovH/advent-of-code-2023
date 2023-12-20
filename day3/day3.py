eecc806c4b9176717976665cdce04f8b3d382041
Hristiyan Pehlivanov <hristiyan.pehlivanov@innoq.com>
2023-12-20 10:04:10 +0100

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,4 +1,7 @@
 
+import itertools
+
+
 def read_file(filename):
     # Create an empty two-dimensional array
     two_dimensional_array = []
@@ -71,87 +74,73 @@ def part1():
     filtered_numbers = sum(numbers)
     print(filtered_numbers)
 
-def findNumber(schematic, row_index, element_index):
-    number = schematic[row_index][element_index]
-   
-    leftIndex = element_index - 1;
-    while leftIndex >= 0 and schematic[row_index][leftIndex].isdigit():
-        number = schematic[row_index][leftIndex] + number
-        leftIndex -= 1
-
-    rightIndex = element_index + 1;
-    while rightIndex < len(schematic[row_index]) and schematic[row_index][rightIndex].isdigit():
-        number = number + schematic[row_index][rightIndex]
-        rightIndex += 1
-
-    return int(number)
-    
-
-def checkForNeighbor(positions, element_index, row_index, schematic, rows, cols):
-    # Iterate over the relative positions
-    for dx, dy in positions:
-        x = row_index + dx
-        y = element_index + dy
-        
-        # Check if the position is within the bounds of the schematic
-        if 0 <= x < rows and 0 <= y < cols:
-            if schematic[x][y].isdigit():
-                return findNumber(schematic, x, y)
-            
-    return 0
-
-def findRatio(schematic, element_index, row_index):
+def findNeighboursWithPos(schematic, elementIndex, rowIndex):
+    neighbours = []
     rows = len(schematic)
     cols = len(schematic[0])
     
     # Define the relative positions of the neighbours
-    toppositions = [
+    positions = [
         (-1, -1),  # top-left
         (-1, 0),   # top
         (-1, 1),   # top-right
-    ]
-
-    bottompositions = [
+        (0, -1),   # left
+        (0, 1),    # right
         (1, -1),   # bottom-left
         (1, 0),    # bottom
         (1, 1)     # bottom-right
     ]
-
-    leftpositions = [
-        (0, -1),   # left
-    ]
     
-    rightpositions = [
-        (0, 1),    # right
-    ]
-
-    topNumber = checkForNeighbor(toppositions, element_index, row_index, schematic, rows, cols)
-    bottomNumber = checkForNeighbor(bottompositions, element_index, row_index, schematic, rows, cols)
-    leftNumber = checkForNeighbor(leftpositions, element_index, row_index, schematic, rows, cols)
-    rightNumber = checkForNeighbor(rightpositions, element_index, row_index, schematic, rows, cols)
+    # Iterate over the relative positions
+    for dx, dy in positions:
+        x = rowIndex + dx
+        y = elementIndex + dy
+        
+        # Check if the position is within the bounds of the schematic
+        if 0 <= x < rows and 0 <= y < cols:
+            neighbours.append({'char':schematic[x][y], 'pos':(x,y) })
     
-    numbers = [num for num in [topNumber, bottomNumber, leftNumber, rightNumber] if num != 0]
-    
-    if len(numbers) == 2:
-         # Calculate the multiplication of the numbers
-        multiplication = 1
-        for num in numbers:
-            multiplication *= num
-        return multiplication
-
-    return 0
-   
+    return neighbours
 
 def part2():
-    schematic = read_file("day3/test_input2")
-
-    numbers = 0
+    schematic = read_file("day3/input2")
+    numbers = []
+    
     for row_index, row in enumerate(schematic):
+        tmp_number = ''
+        neighbours = []
         for element_index, element in enumerate(row):
-            if element == '*':
-                numbers += findRatio(schematic, element_index, row_index)
+            if element.isdigit():
+                tmp_number += element
+                neighbours += findNeighboursWithPos(schematic, element_index, row_index)
+            else:
+                neighbours = list(filter(lambda x: not x.get('char').isdigit() and x.get('char') == '*', neighbours))
+                if len(tmp_number) > 0 and len(neighbours) > 0:
+                    numbers.append({'number':int(tmp_number), 'gear': neighbours[0].get('pos')})
+                tmp_number = ''
+                neighbours = []
 
-    print(numbers)
+        # Check if there is a number at the end of the row
+        neighbours = list(filter(lambda x: not x.get('char').isdigit() and x.get('char') == '*', neighbours))
+        if len(tmp_number) > 0 and len(neighbours) > 0:
+            numbers.append({'number':int(tmp_number), 'gear': neighbours[0].get('pos')})
+
+     # Group numbers by the same 'pos' property
+    grouped_numbers = []
+    numbers.sort(key=lambda x: x['gear'])
+    for pos, group in itertools.groupby(numbers, key=lambda x: x['gear']):
+        group_numbers = [number['number'] for number in group]
+        grouped_numbers.append({'pos': pos, 'numbers': group_numbers})
+
+    
+    grouped_numbers = list(filter(lambda x: len(x['numbers']) == 2, grouped_numbers))
+    sum = 0
+    for entry in grouped_numbers:
+        numbers = entry['numbers']
+        product = numbers[0] * numbers[1]
+        sum += product
+
+    print(sum)
 
 if __name__ == "__main__":
     part2()
